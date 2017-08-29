9ef1f8c123a63dc20cbc0f37607b5d7d6644106f
robinbennedict <robinbennedict@gmail.com>
2017-08-29 08:43:46 +0530

    corrected application properties with appropriate database configurations

--- src/main/java/com/ra/util/GlobalConstants.java
+++ src/main/java/com/ra/util/GlobalConstants.java
@@ -7,13 +7,13 @@ public class GlobalConstants {
 	 */
 	public static final String REST_BASE_URL = "/rest";
 	
-	public static final String REST_ERR_URL = "/error";
+	//public static final String REST_ERR_URL = "/error";
 	
-	public static final String REST_HELLO_URL = "/hello";
+	//public static final String REST_HELLO_URL = "/hello";
 	
-	public static final String REST_LOGOUT_URL = "/logout";
+	//public static final String REST_LOGOUT_URL = "/logout";
 	
-	public static final String LOGIN_PAGE = "/login";
+	//public static final String LOGIN_PAGE = "/login";
 	
 	
 	public static final String FILE_SEPERATOR = "/";
