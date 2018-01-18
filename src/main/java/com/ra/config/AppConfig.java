fba71ef4e9b13470915436edb146bdf25634f9fb
marimuthut <rockmarimuthu@gmail.com>
2018-01-18 21:32:35 +0530

    appconfig

--- src/main/java/com/ra/config/AppConfig.java
+++ src/main/java/com/ra/config/AppConfig.java
@@ -2,8 +2,22 @@ package com.ra.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+ 
 @Configuration
-@ComponentScan
-public class AppConfig {
+@ComponentScan("com.ra.config")
+public class AppConfig extends WebMvcConfigurerAdapter {
+ 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+ 
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:8080")
+		.allowedMethods("POST", "GET", "PUT", "DELETE")
+		.allowedHeaders("Content-Type")
+		.exposedHeaders("header-1")
+		.allowCredentials(false)
+		.maxAge(6000);
+	}
 }
